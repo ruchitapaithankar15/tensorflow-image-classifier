bfb97a3436d5a4eadd4086916fd97b7b1cf01566
ruchitapaithankar15 <ruchitapaithankar@gmail.com>
2022-03-13 13:20:26 -0400

    ADD: image classfier

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -3,3 +3,17 @@ import numpy as np
 import tensorflow as keras
 import matplotlib.pyplot as plt
 
+fashion_mnist = keras.dataset.fashion_mnist
+
+(train_img, train_labels), (test_img, test_labels) = fashion_mnist.load_data()
+
+#print(train_labels[0])
+#print(train_img[0])
+plt.imshow(train_img[0], cmap='gray', vmin=0, vmax=0)
+plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(units=128, activation=tf.nn.relu),
+    keras.layers.Dense(units=10, activation=tf.nn.softmax)
+])
